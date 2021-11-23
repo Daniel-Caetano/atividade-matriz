f84327967bea298981d3f514d828f7eeb27d2983
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2021-11-23 20:09:23 -0300

    Atividade completa

--- mainmatriz.c
+++ mainmatriz.c
@@ -2,11 +2,6 @@
 // Data: 23/11/2021
 // Atividade de Matriz
 
-/*Usando a programação
-modular(procedure and functions) fazer um algoritmo em C para
-    : preencher uma matriz quadrada A de 3x3,
-      de número inteiros
-      randomicamente(aleatório) em seguida calcular o Determinante da matriz A*/
 
 #include <math.h>
 #include <stdio.h>
@@ -18,9 +13,12 @@ int main() {
   srand(time(0));
   int matriz[3][3];
   int matrizEspelho[3][5];
+  double determinante=0;
+  double detAux;
+
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      matriz[i][j] = rand() % 100;
+      matriz[i][j] = rand() % 10;
       matrizEspelho[i][j] = matriz[i][j];
     }
   }
@@ -31,6 +29,7 @@ int main() {
     }
     printf("\n");
   }
+
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 2; j++) {
       matrizEspelho[i][j+3] = matriz[i][j];
@@ -44,5 +43,23 @@ int main() {
     }
     printf("\n");
   }
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+      detAux = matrizEspelho[i][j];
+    }
+    determinante=determinante+detAux;
+    printf("\n");
+  }
+  double diagonalA=(matrizEspelho[0][0]*matrizEspelho[1][1]*matrizEspelho[2][2]);
+  double diagonalB=(matrizEspelho[0][1]*matrizEspelho[1][2]*matrizEspelho[2][3]);
+  double diagonalC=(matrizEspelho[0][2]*matrizEspelho[1][3]*matrizEspelho[2][4]);
+
+  double diagonalD=(matrizEspelho[2][0]*matrizEspelho[1][1]*matrizEspelho[0][2]);
+  double diagonalE=(matrizEspelho[2][1]*matrizEspelho[1][2]*matrizEspelho[0][3]);
+  double diagonalF=(matrizEspelho[2][2]*matrizEspelho[1][3]*matrizEspelho[0][4]);
+
+  determinante=(diagonalA+diagonalB+diagonalC)-(diagonalD-diagonalE-diagonalF);
+
+  printf("\nDeterminante: |%f|\n", determinante);
 
 }
